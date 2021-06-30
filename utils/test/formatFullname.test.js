211da2fbd397839c6cfe4ff6e949a5ca9b67b5f0
Barbara <75448241+BarbaraPakula@users.noreply.github.com>
2021-07-01 00:15:11 +0200

    Add test to formatFullName

---
+++ utils/test/formatFullname.test.js
@@ -0,0 +1,17 @@
+const formatFullname = require('../formatFullname.js');
+const expect = require('chai').expect;
+
+describe('formatFullname', () => {
+  it('should return an error if "fullName" arg is not a string', () => {
+    expect(formatFullname(undefined)).to.equal('Error');
+    expect(formatFullname()).to.equal('Error');
+    expect(formatFullname(12)).to.equal('Error');
+    expect(formatFullname({})).to.equal('Error');
+    expect(formatFullname([])).to.equal('Error');
+    expect(formatFullname(function () {})).to.equal('Error');
+  });
+  it('should return an error if fullName = one word or more than two', () => {
+    expect(formatFullname('John Doe John')).to.equal('Error');
+    expect(formatFullname('John')).to.equal('Error');
+  });
+});
